bdf7ab60bedb010d92fd5d71b21a4810a6160f37
Yash Oswal <yashoswal18@gmail.com>
2022-02-26 15:21:01 +0530

    Update video_converter.py

--- plugins/video_converter.py
+++ plugins/video_converter.py
@@ -125,7 +125,7 @@ async def convert(bot, update):
                 media = sent_.video or sent_.document
                 await sent_.copy(
                   chat_id=Config.LOGCHANNEL,
-                  caption=f"`{media.file_name}`\n\nRenamed for : {cb.from_user.mention}",
+                  caption=f"`{media.file_name}`\n\nRenamed for : {update.from_user.mention}",
                 )
              
              #------END------#
